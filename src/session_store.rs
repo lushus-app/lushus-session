325697605bbaf811ef51a1a29a433254b83a00a1
Brandon Vrooman <brandon.vrooman@gmail.com>
2022-08-19 16:35:35 -0400

    Fix spelling error

--- src/session_store.rs
+++ src/session_store.rs
@@ -1,7 +1,7 @@
-mod redis_sesssion_store;
+mod redis_session_store;
 mod session_key;
 mod session_store;
 
-pub use redis_sesssion_store::RedisSessionStore;
+pub use redis_session_store::RedisSessionStore;
 pub use session_key::SessionKey;
 pub use session_store::SessionStore;
