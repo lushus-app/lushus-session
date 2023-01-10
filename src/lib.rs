f5e36f87fe059282a6ba3151f1b5789e1bc58b90
Brandon Vrooman <brandon.vrooman@gmail.com>
2023-01-09 21:33:18 -0500

    Expose RedisSessionStoreError

--- src/lib.rs
+++ src/lib.rs
@@ -6,5 +6,5 @@ mod storage;
 
 pub use session::{Session, SessionError};
 pub use session_model::SessionModel;
-pub use session_store::{RedisSessionStore, SessionKey, SessionStore};
+pub use session_store::{RedisSessionStore, RedisSessionStoreError, SessionKey, SessionStore};
 pub use storage::{Storage, StorageError};
--- src/session_store.rs
+++ src/session_store.rs
@@ -2,6 +2,6 @@ mod redis_session_store;
 mod session_key;
 mod session_store;
 
-pub use redis_session_store::RedisSessionStore;
+pub use redis_session_store::{RedisSessionStore, StoreError as RedisSessionStoreError};
 pub use session_key::SessionKey;
 pub use session_store::SessionStore;
