8aa56e6d00d32f574a35ffbf3323278621c72245
Brandon Vrooman <brandon.vrooman@gmail.com>
2023-01-09 21:25:51 -0500

    Expose StorageError

--- src/lib.rs
+++ src/lib.rs
@@ -7,4 +7,4 @@ mod storage;
 pub use session::{Session, SessionError};
 pub use session_model::SessionModel;
 pub use session_store::{RedisSessionStore, SessionKey, SessionStore};
-pub use storage::Storage;
+pub use storage::{Storage, StorageError};
